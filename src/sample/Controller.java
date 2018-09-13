447e9399da953d9647072dc8d9da222f1d77b2d5
Luis Fernando <luis2015nando@gmail.com>
2018-09-13 03:57:05 -0300

    Apenas uns botoes, quero aprender a navegar entre telas

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,57 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Label labelPadrao;
+
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private TextField txtSenha;
+
+    @FXML
+    private Button botaoCliente;
+
+    @FXML
+    private Button botaoGerente;
+
+    @FXML
+    private Button botaoCozinheiro;
+
+    @FXML
+    private Button botaoGarcom;
+
+    public Controller() {
+    }
+
+
+    @FXML
+
+    private void acaoDobotao(ActionEvent event) {
+
+
+        labelPadrao.setText("Digite usuario: " + txtUsuario.getText());
+
+    }
+
+    private void acaoGerente(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
