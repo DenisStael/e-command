57e9561987617410bd68be68b09af446df2c5199
Luis Fernando <luis2015nando@gmail.com>
2018-09-17 16:26:57 -0300

    Login da tela gerente provisória, usuario: admin, senha: admin

--- src/sample/TelaLogin/TelaLoginController.java
+++ src/sample/TelaLogin/TelaLoginController.java
@@ -1,25 +1,47 @@
 package sample.TelaLogin;
 
+import javafx.application.Preloader;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import sample.Main;
-import javafx.scene.control.Label;
+
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static javafx.application.Preloader.*;
 
 public class TelaLoginController implements Initializable {
 
     @FXML
     private Label label;
 
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private TextField txtUsuario;
+
+
+    @FXML
+    private Label labelErroLogin;
     public static String tipo;
 
     public void acaoLogar() throws IOException {
-        if(tipo == "Gerente")
-            Main.trocaTela("TelaGerente/telaGerente.fxml");
-        else
+        if (tipo == "Gerente") {
+            if ((txtUsuario.getText().equals("admin")) && (txtSenha.getText().equals("admin"))) {
+                Main.trocaTela("TelaGerente/telaGerente.fxml");
+            } else {
+                labelErroLogin.setText("Usuário ou senha inválidos, tente novamente");
+            }
+        } else if (tipo == "Garçom") {
             System.out.println("Métodos ainda não implementados");
+
+        } else if (tipo == "Cozinheiro") {
+            System.out.println("Métodos ainda não implementados");
+        }
     }
 
     public void acaoRecuperarSenha() {
