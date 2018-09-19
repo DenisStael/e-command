c5c75b0cc75166a2008c652e11671345af717dc8
Denis Stael <denisstael@gmail.com>
2018-09-19 19:33:40 -0300

    Funcionalidade de Montar Cardapio sendo feita

---
+++ src/sample/Prato.java
@@ -0,0 +1,67 @@
+package sample;
+
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Prato {
+    private final SimpleStringProperty nome;
+    private final SimpleStringProperty descricao;
+    private final SimpleFloatProperty preco;
+    private final SimpleIntegerProperty codprato;
+
+    public Prato(String nome, int codprato, String descricao, float preco) {
+        this.nome = new SimpleStringProperty(nome);
+        this.descricao = new SimpleStringProperty(descricao);
+        this.preco = new SimpleFloatProperty(preco);
+        this.codprato = new SimpleIntegerProperty(codprato);
+    }
+
+    public String getNome() {
+        return nome.get();
+    }
+
+    public SimpleStringProperty nomeProperty() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome.set(nome);
+    }
+
+    public String getDescricao() {
+        return descricao.get();
+    }
+
+    public SimpleStringProperty descricaoProperty() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao.set(descricao);
+    }
+
+    public float getPreco() {
+        return preco.get();
+    }
+
+    public SimpleFloatProperty precoProperty() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco.set(preco);
+    }
+
+    public int getCodprato() {
+        return codprato.get();
+    }
+
+    public SimpleIntegerProperty codpratoProperty() {
+        return codprato;
+    }
+
+    public void setCodprato(int codprato) {
+        this.codprato.set(codprato);
+    }
+}
