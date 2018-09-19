5effd4b19fb31c9c91a85d0767d1ca3a423a3411
Luis Fernando <luis2015nando@gmail.com>
2018-09-18 21:22:59 -0300

    resolvendo bugs

---
+++ src/sample/TelaCadastrarProduto/TelaCadastrarProdutoController.java
@@ -0,0 +1,67 @@
+package sample.TelaCadastrarProduto;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import sample.ConexaoBanco;
+import sample.Main;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TelaCadastrarProdutoController {
+
+    ConexaoBanco conexaoBanco = new ConexaoBanco();
+
+    @FXML
+    private TextField txtQtdProduto, txtNomeProduto;
+
+    @FXML
+    TextArea txtDescricao;
+
+    public void acaoVoltar() throws IOException {
+        Main.trocaTela("TelaGerente/telaGerente.fxml");
+    }
+
+    public void acaoCdProduto() {
+        try {
+            PreparedStatement ps = conexaoBanco.connection.prepareStatement
+                    ("INSERT INTO Produto(nome, quantidade, descricao) VALUES(?,?,?);");
+            ps.setString(1, txtNomeProduto.getText());
+            ps.setInt(2, Integer.parseInt(txtQtdProduto.getText()));
+            ps.setString(3, txtDescricao.getText());
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto Cadastrado!");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto!\nErro: " + e);
+        }
+        txtQtdProduto.clear();
+        txtDescricao.clear();
+        txtNomeProduto.clear();
+    }
+
+    public void acaoCancelar(ActionEvent actionEvent) {
+        txtQtdProduto.clear();
+        txtDescricao.clear();
+        txtNomeProduto.clear();
+    }
+
+    public void acaoSalvar(ActionEvent actionEvent) {
+    }
+
+    public void acaoAttProduto(ActionEvent actionEvent) {
+    }
+
+    public void acaoRemoverProduto(ActionEvent actionEvent) {
+        /*try {
+            PreparedStatement ps = conexaoBanco.connection.prepareStatement
+                    ("DELETE FROM Produto WHERE nome = txtNomeProduto ;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
+
