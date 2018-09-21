cc5f9235e047adbd0c9ce8ccc1b12494f410054e
Luis Fernando <luis2015nando@gmail.com>
2018-09-21 03:54:29 -0300

    Pesquisar prato e produto implementados na tela cardápio

--- src/sample/TelaMontarCardapio/TelaMontarCardapioController.java
+++ src/sample/TelaMontarCardapio/TelaMontarCardapioController.java
@@ -2,13 +2,19 @@ package sample.TelaMontarCardapio;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import sample.*;
+
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
 
 public class TelaMontarCardapioController implements Initializable {
@@ -24,33 +30,22 @@ public class TelaMontarCardapioController implements Initializable {
     private TableColumn<Produto, String> colunaProd, colunaDescProd, colunaPrato, colunaDescPrato;
     @FXML
     private TableColumn<Produto, Integer> colunaCodProd, colunaQtdProd, colunaCodPrato, colunaPreco;
+    @FXML
+    private TextField txtNomePrato,txtNomeProduto,txtCodPrato;
 
     @FXML
     public void acaoVoltar() throws IOException {
         Main.trocaTela("TelaGerente/telaGerente.fxml");
     }
     @FXML
-    private void adicionarPrato() {
-
-    }
-    @FXML
-    private void adicionarBebida(){
-
-    }
-    @FXML
-    private void adicionarComplemento(){
-
-    }
-    @FXML
-    private void adicionarOpcional(){
-
-    }
-    @FXML
     private void acaoPesquisarProduto(){
-
+        String sqlPesquisa = "select * from produto where nome ilike '%"+txtNomeProduto.getText()+"%';";
+        tabelaProduto.mostraTabela(tabelaProdutos,colunaProd,colunaDescProd,colunaCodProd,colunaQtdProd,sqlPesquisa);
     }
     @FXML
     private void acaoPesquisarPrato(){
+       String sqlPesquisa = "select * from prato where nome ilike '%"+txtNomePrato.getText()+"%';";
+        tabelaPrato.mostraTabela(tabelaPratos,colunaPrato,colunaDescPrato,colunaCodPrato,colunaPreco,sqlPesquisa);
 
     }
     @FXML
@@ -62,5 +57,45 @@ public class TelaMontarCardapioController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         tabelaProduto.mostraTabela(tabelaProdutos,colunaProd,colunaDescProd,colunaCodProd,colunaQtdProd,sql);
         tabelaPrato.mostraTabela(tabelaPratos,colunaPrato,colunaDescPrato,colunaCodPrato,colunaPreco,sql2);
+    }
+
+    public void acaoAdicionarPrato(ActionEvent actionEvent) {
+        if(!txtCodPrato.getText().isEmpty(){
+            try {
+                //Declaração SQL pra inserção no banco
+                PreparedStatement ps = conexaoBanco.connection.prepareStatement
+                        ("INSERT INTO Produto(nome, quantidade, descricao) VALUES(?,?,?);");
+
+                //Atribui os parâmetros e os valores à declaração SQL criada anteriormente
+                ps.setString(1, txtNomeProduto.getText());
+                ps.setInt(2, Integer.parseInt(txtQtdProduto.getText()));
+                ps.setString(3, txtDescricao.getText());
+
+                ps.executeUpdate();//Executa a declaração SQL
+
+                //Mensagem de Sucesso
+                JOptionPane.showMessageDialog(null, "Produto Cadastrado!");
+
+                //Chama o método mostraTabela da classe TabelaProduto para mostrar os produtos cadastrados no banco na tabela
+                tabelaProduto.mostraTabela(tabelaProdutos,colunaProd,colunaDescricao,colunaCod,colunaQuantidade,sql);
+            } catch (Exception e) {
+                //Mensagem de erro
+                JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto!\nErro: " + e);
+            }
+            //Limpa os campos de texto
+            acaoCancelar();
+
+            //Mensagem caso os campos não tenham todos sidos preenchidos
+        } else JOptionPane.showMessageDialog(null, "Preencha todos os campos!");
+    }
+    }
+
+    public void acaoAdicionarComplemento(ActionEvent actionEvent) {
+    }
+
+    public void acaoAdicionarBebida(ActionEvent actionEvent) {
+    }
+
+    public void acaoAdicionarOpcional(ActionEvent actionEvent) {
     }
 }
