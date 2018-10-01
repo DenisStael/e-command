ea54740eba3b68e7bf671ae6f7f5a7a049f0248c
Denis Stael <denisstael@gmail.com>
2018-10-01 03:03:33 -0300

    Tela da comanda criada (ainda incompleta)

---
+++ src/sample/TelaComanda/TelaComandaController.java
@@ -0,0 +1,11 @@
+package sample.TelaComanda;
+
+import sample.Main;
+import java.io.IOException;
+
+public class TelaComandaController {
+
+    public void acaoVoltar() throws IOException {
+        Main.trocaTela("TelaVisualizarCardapio/telaVisualizarCardapio.fxml");
+    }
+}
