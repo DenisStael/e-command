a06ebb3e3fec34839c1268ba2def4c1a6f23ff20
Denis Stael <denisstael@gmail.com>
2018-10-15 21:23:59 -0300

    Ajustes nas funcionalidades do garçom

--- src/sample/TelaCozinheiro/TelaCozinheiroController.java
+++ src/sample/TelaCozinheiro/TelaCozinheiroController.java
@@ -11,23 +11,40 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import sample.Main;
 import sample.Pedido;
+import sample.TelaGarcom.TelaGarcomController;
+import sample.TelaGarcom.TelaInformacaoGarcomController;
+import sample.Usuario;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TelaCozinheiroController implements Initializable {
+
+    private static Usuario usuario;
     private Pedido pedido = new Pedido();
-    private String sql = "select codpedido,mesa_idmesa,observacao from pedido where cozinheiro_usuario_codusuario is null;";
+    private String sql = "select distinct p.codpedido,p.mesa_idmesa,p.observacao from pedido p, prato pr, pedidoprato pp " +
+            "where p.cozinheiro_usuario_codusuario is null and pp.codpedido = p.codpedido and pp.codprato = pr.codprato " +
+            "and pr.tipo = 'Comida' order by p.codpedido;";
     @FXML
-    private TableView tabelaCozinheiro;
+    private TableView<Pedido> tabelaCozinheiro;
     @FXML
     private TableColumn colunaCodPedido,colunaNumMesa,colunaObservacao;
     public void acaoVoltar() throws IOException {
         Main.trocaTela("TelaLogin/telaLogin.fxml");
     }
 
+    public static Usuario getUsuario() {
+        return usuario;
+    }
+
+    public static void setUsuario(Usuario usuario) {
+        TelaCozinheiroController.usuario = usuario;
+    }
+
     public void acaoInformacaoCozinheiro() throws IOException {
         if(tabelaCozinheiro.getSelectionModel().getSelectedItem() != null){
+            TelaInformacaoCozinheiroController.codPedido = tabelaCozinheiro.getSelectionModel().getSelectedItem().getCodpedido();
             Stage stage = new Stage();
             stage.setTitle("Informações do Pedido");
             Parent root = FXMLLoader.load(getClass().getResource("telaInformacaoCozinheiro.fxml"));
@@ -39,6 +56,6 @@ public class TelaCozinheiroController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        pedido.mostraTabelaGarcom(tabelaCozinheiro,colunaCodPedido,colunaNumMesa,colunaObservacao,sql);
+        pedido.mostraTabela(tabelaCozinheiro,colunaCodPedido,colunaNumMesa,colunaObservacao,sql);
     }
 }
--- src/sample/TelaGarcom/TelaGarcomController.java
+++ src/sample/TelaGarcom/TelaGarcomController.java
@@ -1,6 +1,6 @@
 package sample.TelaGarcom;
 
-import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,19 +9,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import sample.Main;
 import sample.Pedido;
+import sample.Usuario;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TelaGarcomController implements Initializable {
+    private Stage stage = new Stage();
+    private static Usuario usuario;
     private Pedido pedido = new Pedido();
-    private String sql = "select codpedido,mesa_idmesa,observacao from pedido where garcom_usuario_codusuario is null;";
+    private String sql = "select codpedido,mesa_idmesa,observacao from pedido where garcom_usuario_codusuario is null order by codpedido;";
     @FXML
     private TableView<Pedido> tabelaGarcom;
     @FXML
     private TableColumn colunaCodPedido,colunaNumMesa,colunaObservacao;
+
+    public static Usuario getUsuario() {
+        return usuario;
+    }
+
+    public static void setUsuario(Usuario usuario) {
+        TelaGarcomController.usuario = usuario;
+    }
 
     public void acaoVoltar() throws IOException {
         Main.trocaTela("TelaLogin/telaLogin.fxml");
@@ -29,7 +41,6 @@ public class TelaGarcomController implements Initializable {
     public void acaoInformacaoGarcom() throws IOException {
         if(tabelaGarcom.getSelectionModel().getSelectedItem() != null){
             TelaInformacaoGarcomController.codPedido = tabelaGarcom.getSelectionModel().getSelectedItem().getCodpedido();
-            Stage stage = new Stage();
             stage.setTitle("Informações do Pedido");
             Parent root = FXMLLoader.load(getClass().getResource("telaInformacaoGarcom.fxml"));
             Scene scene = new Scene(root);
@@ -40,6 +51,14 @@ public class TelaGarcomController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        pedido.mostraTabelaGarcom(tabelaGarcom,colunaCodPedido,colunaNumMesa,colunaObservacao,sql);
+        pedido.mostraTabela(tabelaGarcom,colunaCodPedido,colunaNumMesa,colunaObservacao,sql);
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                stage.close();//Fecha a aplicação
+                pedido.mostraTabela(tabelaGarcom,colunaCodPedido,colunaNumMesa,colunaObservacao,sql);
+            }
+        });
     }
 }
